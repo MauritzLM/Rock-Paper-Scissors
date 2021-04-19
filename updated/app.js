d2858dcfe91cba225e7d7b35932052ce88eca198
Mauritz Marais <mauritz303@gmail.com>
2021-04-19 16:18:12 +0200

    added game functionality

--- updated/app.js
+++ updated/app.js
@@ -9,8 +9,10 @@ const computer = {
 
 let playerSelection;
 let computerSelection;
-const score = document.getElementById('score')
+const score = document.getElementById('score');
 const buttons = document.querySelectorAll('button');
+const result = document.getElementById('result');
+// const winningScore = 5;
 
 
 buttons.forEach((button) => {
@@ -33,59 +35,65 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    if (user.score !== 5 && computer.score !== 5) {
+        if (playerSelection === 'rock' && computerSelection === 'scissors') {
+            const result1 = document.createElement('p')
+            result1.textContent = 'You win this round! Rock beats Scissors'
+            score.appendChild(result1)
+            user.score += 1;
+            user.display.textContent = user.score;
+            winner()
+        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+            const result2 = document.createElement('p')
+            result2.textContent = 'You lose this round! Rock beats Scissors'
+            score.appendChild(result2)
+            computer.score += 1;
+            computer.display.textContent = computer.score;
+            winner()
+        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+            const result3 = document.createElement('p')
+            result3.textContent = 'You win this round! Paper beats Rock'
+            score.appendChild(result3)
+            user.score += 1;
+            user.display.textContent = user.score;
+            winner()
+        } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+            const result4 = document.createElement('p')
+            result4.textContent = 'You lose this round! Paper beats Rock'
+            score.appendChild(result4)
+            computer.score += 1;
+            computer.display.textContent = computer.score;
+            winner()
+        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+            const result5 = document.createElement('p')
+            result5.textContent = 'You win this round! Scissors beats Paper'
+            score.appendChild(result5)
+            user.score += 1;
+            user.display.textContent = user.score;
+            winner()
+        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+            const result6 = document.createElement('p')
+            result6.textContent = 'You lose this round! Scissors beats Paper'
+            score.appendChild(result6)
+            computer.score += 1;
+            computer.display.textContent = computer.score;
+            winner()
+        } else {
+            const result7 = document.createElement('p')
+            result7.textContent = 'Tie!'
+            score.appendChild(result7)
 
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        const result1 = document.createElement('p')
-        result1.textContent = 'You win this round! Rock beats Scissors'
-        score.appendChild(result1)
-        user.score += 1;
-        user.display.textContent = user.score;
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        const result2 = document.createElement('p')
-        result2.textContent = 'You lose this round! Rock beats Scissors'
-        score.appendChild(result2)
-        computer.score += 1;
-        computer.display.textContent = computer.score;
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        const result3 = document.createElement('p')
-        result3.textContent = 'You win this round! Paper beats Rock'
-        score.appendChild(result3)
-        user.score += 1;
-        user.display.textContent = user.score;
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        const result4 = document.createElement('p')
-        result4.textContent = 'You lose this round! Paper beats Rock'
-        score.appendChild(result4)
-        computer.score += 1;
-        computer.display.textContent = computer.score;
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        const result5 = document.createElement('p')
-        result5.textContent = 'You win this round! Scissors beats Paper'
-        score.appendChild(result5)
-        user.score += 1;
-        user.display.textContent = user.score;
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        const result6 = document.createElement('p')
-        result6.textContent = 'You lose this round! Scissors beats Paper'
-        score.appendChild(result6)
-        computer.score += 1;
-        computer.display.textContent = computer.score;
-    } else {
-        const result7 = document.createElement('p')
-        result7.textContent = 'Tie!'
-        score.appendChild(result7)
-
+        }
     }
 }
 
-function result() {
-    if (userScore > computerScore) {
-        return 'You win the game!'
-    } else if (userScore < computerScore) {
-        return 'Aww you lost'
-    } else {
-        return `It's a tie!`
+
+
+function winner() {
+    if (user.score === 5) {
+        result.textContent = 'You Win!'
+    }
+    else if (computer.score === 5) {
+        result.textContent = 'Aww you lost...'
     }
 }
-
-
